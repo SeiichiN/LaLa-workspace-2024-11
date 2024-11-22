a1160717a966f4191238ab7525b71fa9781de6ed
SeiichiN <billie175@gmail.com>
2024-11-22 09:12:35 +0900

    Janken Game end syori.

--- janken/src/main/Main.java
+++ janken/src/main/Main.java
@@ -3,16 +3,21 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		while(true) {
-			janken();
-		}
+		while (true) {
+			int num = janken();
+			if (num == 9) {
+				break;
+			}
+		} 
+		System.out.println("終了します");
 	}
 	
-	public static void janken() {
+	public static int janken() {
 		String[] hand = {"グー", "チョキ", "パー"};
 		int com = new java.util.Random().nextInt(3);
-		System.out.print("0:グー 1:チョキ 2:パー > ");
+		System.out.print("0:グー 1:チョキ 2:パー 9:終了 > ");
 		int user = new java.util.Scanner(System.in).nextInt();
+		if (user == 9) { return 9; }
 		String result = "";
 		if (user == com) {
 			result = "あいこ";
@@ -21,8 +26,9 @@ public class Main {
 		} else {
 			result = "わたしの勝ち";
 		}
-		System.out.println("わたし:" + hand[com]);
-		System.out.println("ユーザー:" + hand[user]);
+		System.out.print("わたし:" + hand[com]);
+		System.out.println(" ユーザー:" + hand[user]);
 		System.out.println(result);
+		return 0;
 	}
 }

14014c34a5c6351b522838ad495468e3ccca22a2
SeiichiN <billie175@gmail.com>
2024-11-28 08:00:10 +0900

    field_overload-junbi modified.

---
+++ field_overload-junbi/src/test/Hero.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Hero {
+	String name = "名無し";
+	
+	public void run() {
+		System.out.println(this.name + "は逃げ出した。");
+	}
+}
---
+++ field_overload-junbi/src/test/SuperHero.java
@@ -0,0 +1,10 @@
+package test;
+
+public class SuperHero extends Hero {
+	String name = "スーパー名無し";
+	
+	@Override
+	public void run() {
+		System.out.println(this.name + "はすばやく逃げ出した。");
+	}
+}

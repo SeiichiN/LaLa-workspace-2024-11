0f5bfbdca8e8a404b2a4c2d677546b3fd70d0c0e
SeiichiN <billie175@gmail.com>
2024-11-28 09:13:06 +0900

    project name modified.

--- field_override-junbi/src/test/SuperHero.java
+++ field_override-junbi/src/test/SuperHero.java
@@ -1,15 +1,10 @@
-package rpg;
+package test;
 
 public class SuperHero extends Hero {
-	String name;
-	public SuperHero(String name) {
-		super(name);
-	}
-	public SuperHero() {
-		this("匿名");
-	}
+	// String name = "スーパー名無し";
+	
+	@Override
 	public void run() {
 		System.out.println(this.name + "はすばやく逃げ出した。");
-		super.run();
 	}
 }

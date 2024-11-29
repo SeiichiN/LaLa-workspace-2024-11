ea9d7f5522db53d80a044490f04d44f699c7b37a
SeiichiN <billie175@gmail.com>
2024-11-29 22:27:25 +0900

    player move

--- adventure/src/main/Main.java
+++ adventure/src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Game game = new Game();
+		Scanner scan = new Scanner(System.in);
 		Goblin g = new Goblin("goblin");
 		g.setLocation(game);
 		g.setSelfOnMap(game);
@@ -18,10 +21,20 @@ public class Main {
 		
 		Player p = new Player("太郎");
 		p.setLocation(game);
+		p.printNowPlace();
 
-		game.printMap();  // mapを表示
-		p.move("w");  // ←に
-		p.move("n");  // ↑に
+		// game.printMap();  // mapを表示
+		
+		outLoop:
+		while (true) {
+			System.out.print("wens:移動 q:終了 > ");
+			String dir = scan.nextLine();
+			switch (dir) {
+			case "q" -> { break outLoop; }
+			case "w","e","n","s" -> { p.move(dir, game); }
+			}
+		}
+		System.out.println("終了します。");
 	}
 
 }
--- adventure/src/main/Player.java
+++ adventure/src/main/Player.java
@@ -8,11 +8,33 @@ public class Player extends GameLocation {
 		this.name = name;
 		this.hp = 100;
 	}
-	public void move(String dir) {
-		
+	public void printNowPlace() {
+		System.out.println(this.name + "[" + this.y + ":" + this.x + "]");
+	}
+	public void move(String dir, Game g) {
+		switch (dir) {
+		case "w" -> { moveLeft(); }
+		case "e" -> { moveRight(g); }
+		case "n" -> { moveUp(); }
+		case "s" -> { moveDown(g); }
+		}
+		printNowPlace();
 	}
 	public void moveLeft() {
-		// xを-1
-		// もしxがマイナスなら x=0 とする
+		this.x -= 1;
+		if (this.x < 0) this.x = 0;
 	}
+	public void moveRight(Game g) {
+		this.x += 1;
+		if (this.x >= g.XSIZE) this.x = g.XSIZE - 1;
+	}
+	public void moveUp() {
+		this.y -= 1;
+		if (this.y < 0) this.x = 0;
+	}
+	public void moveDown(Game g) {
+		this.y += 1;
+		if (this.y >= g.YSIZE) this.y = g.YSIZE - 1;
+	}
+	
 }
